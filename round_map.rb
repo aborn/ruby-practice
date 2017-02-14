c042689a61aa7fcce014b03bac62bf1a9189288b
guobao.jiang <guobao.jiang@dianping.com>
2017-02-14 11:14:24 +0800

    finished

--- round_map.rb
+++ round_map.rb
@@ -1,21 +1,44 @@
-$roundMap = Hash.new
+# coding: utf-8
+
 
 class RoundMap
+  RoundMapMaxSize = 2 * 1000
+  @@roundMap = Hash.new
 
   def initialize()
   end
 
   def put(key, value)
-    $roundMap[key] = value
+    currentIndex = key.divmod(RoundMapMaxSize)[1]   # 取余
+    @@roundMap[currentIndex] = value
+    for i in 0...RoundMapMaxSize
+      if (i - currentIndex).abs >= RoundMapMaxSize/2
+        @@roundMap.delete(i)
+      end
+    end
   end
 
   def get(key)
-    return $roundMap[key]
+    if @@roundMap.has_key?(key)
+      return @@roundMap[key]
+    end
+
+    return -1
   end
 end
 
 if __FILE__ == $0
   rm = RoundMap.new
-  rm.put(1, 3)
-  print "value = #{rm.get(1)}\n"
+  print "**** map[0] = #{rm.get(0)} \n"
+
+  for j in 0...5
+    p j
+  end
+
+  for i in 0...2005
+    rm.put(i, i+1)
+    if i == 2002
+      print "map[0]= #{rm.get(0)}, map[1]= #{rm.get(1)}, map[2]= #{rm.get(2)}, map[3]=#{rm.get(3)} \n"
+    end
+  end
 end
