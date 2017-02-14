bee118dd57d0710c84043f23111e5a74b3336633
guobao.jiang <guobao.jiang@dianping.com>
2017-02-14 10:35:13 +0800

    commit by magit in emacs@2017-02-14 10:35:12

--- round_map.rb
+++ round_map.rb
@@ -1,3 +1,21 @@
-
 $roundMap = Hash.new
-print "hello ruby\n"
+
+class RoundMap
+
+  def initialize()
+  end
+
+  def put(key, value)
+    $roundMap[key] = value
+  end
+
+  def get(key)
+    return $roundMap[key]
+  end
+end
+
+if __FILE__ == $0
+  rm = RoundMap.new
+  rm.put(1, 3)
+  print "value = #{rm.get(1)}\n"
+end
